61cb2775cd5abeb8e46c86d5dd32d6a9fb49b333
JeongHyeon Lee <sjeonghyeonz@gmail.com>
2021-05-14 16:53:55 +0900

    refactor: Modify to fit OOP

--- PDParser/rule.py
+++ PDParser/rule.py
@@ -1,21 +1,21 @@
 class Rule:
     def __init__(self, no: int, s: str):
         rule = s.replace(' ', '').split('→')
-        self.id = no
-        self.LHS = rule[0]
-        self.RHS = rule[1]
+        self.__id = no
+        self.__LHS = rule[0]
+        self.__RHS = rule[1]
 
     def get_id(self):
-        return self.id
+        return self.__id
 
     def get_RHS_len(self):
-        return len(self.RHS)
+        return len(self.__RHS)
 
     def get_a_RHS(self, idx: int):
-        return self.RHS[idx]
+        return self.__RHS[idx]
 
     def get_RHS(self):
-        return self.RHS
+        return self.__RHS
 
     def get_LHS(self):
-        return self.LHS
+        return self.__LHS
