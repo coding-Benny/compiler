9d694969d3285a6887bbc96d1b98672f2d0d8886
JeongHyeon Lee <sjeonghyeonz@gmail.com>
2021-05-16 21:11:00 +0900

    refactor: Change parameter type

--- PDParser/parsing_table.py
+++ PDParser/parsing_table.py
@@ -9,5 +9,5 @@ class ParsingTable:
     def insert_rule(self, r: int, c: int, p: Rule):
         self.__table[r][c] = p.get_RHS()
 
-    def get_rule(self, r: str, c: str):
+    def get_rule(self, r: int, c: int):
         return self.__table[r][c]
