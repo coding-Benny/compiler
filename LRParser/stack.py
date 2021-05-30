9907ee8f24538a31c7d1f2af2feaf332215c7721
JeongHyeon Lee <sjeonghyeonz@gmail.com>
2021-05-30 12:12:42 +0900

    refactor: Remove unused codes

--- LRParser/stack.py
+++ LRParser/stack.py
@@ -2,11 +2,6 @@ class Stack:
     def __init__(self):
         self.__content = []
 
-    def expand(self, s: str):
-        self.pop()
-        for element in s[::-1]:
-            self.push(element)
-
     def push(self, c: str):
         self.__content.append(c)
 
@@ -15,12 +10,6 @@ class Stack:
 
     def get_stack_top(self):
         return self.__content[-1]
-
-    def is_empty(self):
-        if not self.__content:
-            return True
-        else:
-            return False
 
     def get_stack(self):
         return self.__content
